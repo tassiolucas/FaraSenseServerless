b8da6134b531fc1913331c7e45796e140c19044e
Tássio Lucas <tassiolucas@live.com>
2018-09-28 18:29:44 -0300

    -Simplifica o código ServerLess e adiciona GET por faixa de tempo

--- create.js
+++ create.js
@@ -1,26 +1,32 @@
 'use strict';
 
+const uuid = require('uuid');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.get = (event, context, callback) => {
+module.exports.create = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+
   const params = {
     TableName: process.env.CURRENT_SENSOR_TABLE,
-    Key: {
-      id: event.pathParameters.id,
-    },
+    Item: {
+      id: body.id,
+      timestamp: new Date(body.timestamp).toISOString(),
+      amper: Number(body.amper),
+      milliamper: Number(body.milliamper),
+      power: Number(body.power)
+    }
   };
 
-  // fetch todo from the database
-  dynamoDb.get(params, (error, result) => {
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
     // handle potential errors
     if (error) {
-      console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todo item.',
+        body: error.message,
       });
       return;
     }
@@ -28,7 +34,7 @@ module.exports.get = (event, context, callback) => {
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item),
+      body: JSON.stringify(params.Item),
     };
     callback(null, response);
   });
---
+++ get.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+
+  const params = {
+    TableName: process.env.CURRENT_SENSOR_TABLE,
+    KeyConditionExpression: 'id = :ident and #timestamp between :start and :end',
+    ExpressionAttributeValues: {
+      ':ident': Number(event.pathParameters.id),
+      ':start': new Date(Number(event.pathParameters.start)).toISOString(),
+      ':end': new Date(Number(event.pathParameters.end)).toISOString()
+    },
+    ExpressionAttributeNames: {
+      "#timestamp": "timestamp"
+    }
+  };
+
+  // fetch todo from the database
+  dynamoDb.query(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: error.message,
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+
+};
